d05a7ce4bd09a8175ac5d0fa9b20430280f3d2ab
dchangeux18 <dchangeux@gmail.com>
2015-09-07 15:25:08 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,31 @@
+/////////////////////////////////////////////////////////////////
+//Danelia Changeux
+//9-3-2015
+//Welcome Class Java Program 
+//
+//  first compile the program 
+//      javac WelcomeClass.java
+//  run the program 
+//      java WelcomeClass
+
+//  define a class
+public class WelcomeClass{
+
+//  add main method 
+  public static void main(String[ ] args) {
+      
+// print the statement with the box around it 
+   System.out.println("-----------");
+   System.out.println("| WELCOME |");
+   System.out.println("-----------");
+   System.out.println("  ^  ^  ^  ^  ^  ^");
+   System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+   System.out.println("<-D--M--C--4--1--8->");
+   System.out.println(" \\/ \\/ \\/ \\/ \\/ \\ /");
+   System.out.println("  v  v  v  v  v  v");
+   System.out.println(" I am half Hatian and Half Salvadorean and I know how to speak spanish and creole.");
+   
+   
+   //test/testtest
+  }
+}
